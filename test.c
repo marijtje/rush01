670bd276a3a9ef8491f9dcbee78df979d5cdbc42
Marijtje <marijtjedeboer@ziggo.nl>
2021-06-12 20:51:26 +0200

    t

---
+++ test.c
@@ -0,0 +1,24 @@
+int x [4][4];
+
+col1up[0][0]
+col2up[0][1]
+col3up[0][2]
+col4up[0][3]
+
+col1down[3][0]
+col2down[3][1]
+col3down[3][2]
+col4down[3][3]
+
+row1left[0][0]
+row2left[1][0]
+row3left[2][0]
+row4left[3][0]
+
+row1right
+row2right
+row3right
+row4right
+
+
+void malloc (16*(int))
